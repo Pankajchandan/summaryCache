24253b9b767e0a50633fcb13e1a1239a8226efe3
pankaj.mest@gmail.com <pankaj.mest@gmail.com>
2017-10-05 14:15:23 -0700

    bug in proxy1Trial

--- lib.py
+++ lib.py
@@ -5,6 +5,7 @@ import math
 import socket
 import sys
 import pickle
+from threading import Thread
 
 ## method to make a bloom filter , list_param = list of things, 
 ## size = size of filter, no_of_hashes = no of hashes
@@ -14,11 +15,12 @@ def make_filter(list_param, size, no_of_hashes):
     hash_num = 41
     print "building own summary..."
     for url in list_param:
-        for i in range(int(no_of_hashes)):
-            bit = mmh3.hash(url, hash_num+i) % int(size)
+        for i in range(no_of_hashes):
+            bit = mmh3.hash(url, hash_num+i) % size
             bit_array[bit] = 1
     
     return bit_array
+
 
 ## method to check if thing is present in bloom filter , bit_array = bloom filter, 
 ## size = size of filter(no of bits), no_of_hashes = no of hashes, url = object to check
@@ -26,20 +28,20 @@ def check_filter(bit_array, size, no_of_hashes, url):
     bit_list = []
     hash_num = 41
     for i in range(int(no_of_hashes)):
-        bit_list.append(mmh3.hash(url, hash_num+i) % int(size))
+        bit_list.append(mmh3.hash(url, hash_num+i) % size)
     for bit in bit_list:
         if bit_array[bit] == False:
             return False
     return True
 
+
 ## method to iterate check_filter over all the proxies
 def check_filter_list(filter_dict, url):
-    ## list all the proxies that might have the URl 
+    ## list all the proxies that might have the URl
     proxy_true_list = []
     if len(filter_dict) is 0:
         print "empty summary cache..."
         return proxy_true_list
-
     print "checking in summary cache dictionary"
     for key, value in filter_dict.items():
         if check_filter(value[0], value[1], value[2], url):
@@ -47,12 +49,14 @@ def check_filter_list(filter_dict, url):
             proxy_true_list.append(key)
     return proxy_true_list
 
+
 ##method to calculate size of a bloom filter and no of hashes required given the no of items and FP probability
 def size_hash_calc(items,prob):
     size = math.ceil((items * math.log(prob)) / math.log(1.0 / (pow(2.0, math.log(2.0)))))
     hashes = round(math.log(2.0) * size / items)
     print "size and no of hashes calculated"
     return size, hashes
+
 
 ## method to fetch all the proxies avilable 
 ## returns a list
@@ -63,6 +67,25 @@ def fetch_proxy_list():
     print "proxy list fetched from proxy_list.txt"
     return proxy_list[:-1]
 
+
+## workers for send_filter_details
+def send_filter_detail_worker(item,index_list_srl):
+    ##connect to the server and send the index_list_srl
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (item, 9000)
+    print ('connecting to %s port %s' % server_address)
+    try:
+        sock.connect(server_address)
+        sock.sendall(index_list_srl.encode('utf-8'))
+        sock.close()
+        print "sent own summary to: ",item,"\n"
+    except socket.error as msg:
+        print "proxy offline: ",item,"\n"
+        print "reason: ", msg,"\n"
+        pass
+    sock.close()
+    
+
 ## send an object to all remote proxies
 def send_filter_details(bit_array, size, no_of_hashes):
     ## fetch list of proxies to send
@@ -71,21 +94,10 @@ def send_filter_details(bit_array, size, no_of_hashes):
     index_list = [bit_array, size, no_of_hashes]
     ## serialize the object into string using pickle
     index_list_srl = pickle.dumps(index_list)
-    ## Connect the socket to the port where the server is listening
+    
+    ## Connect the socket to the port where the server is listeningusing threads
     print "sending own summary to all the proxies online"
     for item in proxy_list:
-        ## Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (item, 9000)
-        print ('connecting to %s port %s' % server_address)
-        try:
-            sock.connect(server_address)
-            sock.sendall(index_list_srl.encode('utf-8'))
-            sock.close()
-            print "sent own summary to: ",item
-        except socket.error as msg:
-            print "proxy offline: ",item
-            print msg
-        ##close the socket
-        sock.close()
+        thread_send_filter_details_worker = Thread(target = send_filter_detail_worker, args = (item,index_list_srl))
+        thread_send_filter_details_worker.start()
 
---
+++ proxy1Trial.py
@@ -0,0 +1,224 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import BaseHTTPServer
+import os
+import socket
+from threading import Thread
+import sys
+import pickle
+from bitarray import bitarray
+from lib import check_filter_list
+import time
+
+
+# In[2]:
+
+
+def load_backup(filename):
+    #print "loading summary cache from backup file: ",filename
+    with open(filename, 'rb') as save:
+         return pickle.load(save)
+
+
+# In[3]:
+
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+        
+    # Handle a GET request.
+    def do_GET(self):
+        global filter_dict
+        try:
+            sentflag = 0
+            falsepositive = 0
+            ## figure out cache folder name
+            cache_name = self.path.split("/")[2]
+            
+            # Figure out what exactly is being requested. This is the full path where file should exist
+            full_path = os.getcwd()+"/cache/"+cache_name+"/"+"index.html"
+            
+            # It doesn't exist...i.e not in the localcache
+            if not os.path.exists(full_path):
+                print "resource not found in local cache...\n"
+                print "checking summary caches...\n"
+                proxy_true_list = check_filter_list(filter_dict, cache_name)
+                
+                if len(proxy_true_list) is 0:
+                    print "no hits found in summary cache...\nsending request to default gateway...\n"
+                    content = "serving from internet"
+                    self.send_content(content)
+                else:
+                    for proxy in proxy_true_list:
+                        if sentflag ==1:
+                            sentflag = 0
+                            break
+                        else:
+                            sentflag = sentflag + self.request_proxy(proxy,cache_name)
+                
+            # ...if the file exists in cache
+            elif os.path.isfile(full_path):
+                print "resource found in local cache...\n"
+                self.handle_file(full_path)
+            
+            # ...it's something we don't handle.
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+                
+        # Handle errors.
+        except Exception as msg:
+            self.handle_error(msg)
+    
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+    
+    Error_Page = """        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content)
+    
+    # Handle unknown objects.
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+
+    # Send actual content.
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+        
+    def request_proxy(self, proxy, cache_name):
+        proxies = {
+          'http': 'http://'+proxy+':8080',
+          'https': 'http://'+proxy+':8080',
+        }
+        response = requests.get(self.path,proxies=proxies)
+        if response.status_code == "404":
+            falsepositive = falsepositive+1
+            print "false positive from ",proxy, "\n"
+            return 0
+        else:
+            print "status code: ", response.status_code
+            self.send_content(response.text.encode('utf-8'))
+            print "content: ", response.text, "\n"
+            print "content sent to client....\n"
+            return 1
+
+
+# In[4]:
+
+
+# Run local tcp sever at port 9000
+def run_port(sock,filter_dict):
+    print "listening to TCP connections at port 9000......\n"
+    while True:
+        connection, client_address = sock.accept()
+        print "connection from: ", client_address, "\n"
+        thread_add_filter = Thread(target = add_filter, args = (connection, client_address))
+        thread_add_filter.start()
+        thread_add_filter.join()
+    
+# method to add filters to dictionary
+def add_filter(connection, client_address):
+    global filter_dict
+    data = connection.recv(8192)
+    index_list = data.decode('utf-8')
+    index_data = pickle.loads(index_list)
+    filter_dict[client_address[0]] = index_data
+    connection.close()
+
+
+# In[5]:
+
+
+def run_servers():
+    
+    global filter_dict
+    global server
+    global sock
+    
+    print "initial filterdict has: ", filter_dict,"\n"
+        
+    ##check backup file
+    if os.path.isfile("backup.pickle"):
+        print "initializing history....\n"
+        filter_dict = load_backup("backup.pickle")
+        print "history: ",filter_dict,"\n"
+        
+    ##run tcp server
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ('', 9000)
+    sock.bind(server_address)
+    sock.listen(5)
+    print "port opened on 9000...\n"
+        
+    ##spawn a new thread to listen in a loop
+    thread_run_port = Thread(target = run_port, args = (sock,filter_dict))
+    thread_run_port.start()
+        
+    ##run http server
+    print "running http server on port 8080...\n "
+    server.serve_forever()
+
+
+# In[6]:
+
+
+if __name__ == '__main__':
+    
+    ##declare filter dictionary
+    filter_dict =  {}
+    
+    ##initialize tcp server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    ##initialize http server
+    serverAddress = ('', 8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
+    
+    try:
+        run_servers()
+    
+    ##handle exceptions
+    except KeyboardInterrupt:
+        print "writing to file backup.pickle : ", filter_dict, "\n"
+        print "saving filter before terminating...\n"
+        with open("backup.pickle", 'wb') as save:
+            pickle.dump(filter_dict,save)
+        sock.close()
+        server.socket.close()
+        print "port terminated at 9000...\n"
+        print "http server terminated...\n"
+        pass
+
+
+# In[10]:
+
+
+sock.close()
+
+
+# In[ ]:
+
+
+
+
